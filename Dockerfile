92b3131dafc7d969e7ccea56048ddab5dd3cf971
Ethan <940941188@qq.com>
2018-04-19 19:37:21 +0800

    add webpack build

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,33 @@
 FROM ubuntu:xenial
 
+# Install NodeJS
+RUN apt-get update && apt-get -y install git build-essential curl
+RUN curl -sL https://deb.nodesource.com/setup_7.x | bash -
+RUN apt-get install -y nodejs
+
 # Install Nginx
 RUN apt-get update
 RUN apt-get -y install nginx && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-# Copy dist
+# Install Yarn
+RUN curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list && \
+    apt-get update && \
+    apt-get install yarn
+
+# Build dist
 ADD . /root/webapp
 WORKDIR /root/webapp
+RUN yarn install
+RUN NODE_ENV=production yarn run build
 
 # Copy dist
 COPY nginx.conf /etc/nginx/conf.d/default.conf
-RUN rm -f /etc/nginx/sites-enabled/* && cp -r ./ /usr/share/nginx/html
+RUN rm -f /etc/nginx/sites-enabled/* && \
+    cp -r assets/* /usr/share/nginx/html && \
+    cp -r dist/* /usr/share/nginx/html
 
 EXPOSE 80
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,79 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var path = require('path')
-var webpack = require('webpack')
-
-module.exports = {
-  entry: './src/js/main.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
-  },
-};
+module.exports = [
+  {
+    entry: {
+      global: './src/css/global.css',
+      homePage: './src/css/homePage.css',
+      joinus: './src/css/joinus.css',
+      service: './src/css/service.css',
+      aboutus: './src/css/aboutus.css',
+      main: './src/js/main.js'
+    },
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      publicPath: '/',
+      filename: '[name].[chunkhash].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  minimize: true
+                }
+              }
+            ]
+          })
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: '[name][hash].[ext]',
+                publicPath: '/assets',
+                outputPath: './assets'
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new webpack.HashedModuleIdsPlugin(),
+      new ExtractTextPlugin("css/[name].[contenthash].css"),
+      new HtmlWebpackPlugin({
+        chunks: ['main', 'global', 'homePage'],
+        filename: 'index.html',
+        template: './src/index.html',
+      }),
+      new HtmlWebpackPlugin({
+        chunks: ['global', 'joinus'],
+        filename: 'joinus.html',
+        template: './src/joinus.html'
+      }),
+      new HtmlWebpackPlugin({
+        chunks: ['global', 'service'],
+        filename: 'service.html',
+        template: './src/service.html'
+      }),
+      new HtmlWebpackPlugin({
+        chunks: ['global', 'aboutus'],
+        filename: 'aboutus.html',
+        template: './src/aboutus.html'
+      })
+    ]
+  }
+];
